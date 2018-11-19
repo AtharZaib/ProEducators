257860952f2e510a497cc6bb5d456822250ec819
AtharZaib <syed.athar2k14@gmail.com>
2018-11-20 04:24:40 +0500

    Test Website

---
+++ js/app.js
@@ -0,0 +1,115 @@
+
+var dots = document.getElementById("dots");
+var dots1 = document.getElementById("dots1");
+var dots2 = document.getElementById("dots2");
+var dots3 = document.getElementById("dots3");
+var dots4 = document.getElementById("dots4");
+var dots5 = document.getElementById("dots5");
+
+var moreText = document.getElementById("more");
+var moreText1 = document.getElementById("more1");
+var moreText2 = document.getElementById("more2");
+var moreText3 = document.getElementById("more3");
+var moreText4 = document.getElementById("more4");
+var moreText5 = document.getElementById("more5");
+
+var btnText = document.getElementById("myBtn");
+var btnText1 = document.getElementById("myBtn1");
+var btnText2 = document.getElementById("myBtn2");
+var btnText3 = document.getElementById("myBtn3");
+var btnText4 = document.getElementById("myBtn4");
+var btnText5 = document.getElementById("myBtn5");
+
+function myFunction() {
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+
+
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+
+
+
+    }
+
+}
+function myFunction1() {
+    if (dots1.style.display === "none") {
+        dots1.style.display = "inline";
+        btnText1.innerHTML = "Read more";
+        moreText1.style.display = "none";
+
+
+    } else {
+        dots1.style.display = "none";
+        btnText1.innerHTML = "Read less";
+        moreText1.style.display = "inline";
+
+
+    }
+}
+
+function myFunction2() {
+    if (dots2.style.display === "none") {
+        dots2.style.display = "inline";
+        btnText2.innerHTML = "Read more";
+        moreText2.style.display = "none";
+
+    } else {
+        dots2.style.display = "none";
+        btnText2.innerHTML = "Read less";
+        moreText2.style.display = "inline";
+
+
+    }
+}
+function myFunction3() {
+
+    if (dots3.style.display === "none") {
+        dots3.style.display = "inline";
+        btnText3.innerHTML = "Read more";
+        moreText3.style.display = "none";
+
+    } else {
+        dots3.style.display = "none";
+        btnText3.innerHTML = "Read less";
+        moreText3.style.display = "inline";
+
+
+    }
+}
+function myFunction4() {
+
+    if (dots4.style.display === "none") {
+        dots4.style.display = "inline";
+        btnText4.innerHTML = "Read more";
+        moreText4.style.display = "none";
+
+    } else {
+        dots4.style.display = "none";
+        btnText4.innerHTML = "Read less";
+        moreText4.style.display = "inline";
+
+
+    }
+}
+function myFunction5() {
+    if (dots5.style.display === "none") {
+        dots5.style.display = "inline";
+        btnText5.innerHTML = "Read more";
+        moreText5.style.display = "none";
+
+    } else {
+        dots5.style.display = "none";
+        btnText5.innerHTML = "Read less";
+        moreText5.style.display = "inline";
+
+
+    }
+}
+
+
